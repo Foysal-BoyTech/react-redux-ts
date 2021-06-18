7e01d54e54d35b6323b21eef790fc66b203f1a8f
Foysal Mohammad <foysal@earlblue.com>
2021-06-18 21:20:32 +0100

    redux added to index with provider

--- react-redux-ts/src/App.tsx
+++ react-redux-ts/src/App.tsx
@@ -1,6 +1,9 @@
 import './App.css'
+import {useSelector} from 'react-redux'
 
 function App() {
+
+   const state = useSelector(state => state.)
   return (
     <div className="App">
       <h2>Hello Redux ts</h2>
