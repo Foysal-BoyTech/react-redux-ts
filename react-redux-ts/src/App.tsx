de9b25841995162fc3414316c0ae434722b197b4
Foysal Mohammad <foysal@earlblue.com>
2021-06-18 21:55:06 +0100

    app is working

--- react-redux-ts/src/App.tsx
+++ react-redux-ts/src/App.tsx
@@ -1,12 +1,25 @@
 import './App.css'
-import {useSelector} from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { actionCreators } from './state'
+import { State } from './state/reducers/index'
+//import reducers from './state/reducers'
 
 function App() {
+  const dispatch = useDispatch()
 
-   const state = useSelector(state => state.)
+  const { depositMoney, withdrawMoney, bankrupt } = bindActionCreators(
+    actionCreators,
+    dispatch,
+  )
+  const amount = useSelector((state: State) => state.bank)
+
   return (
     <div className="App">
-      <h2>Hello Redux ts</h2>
+      <h2>{amount}</h2>
+      <button onClick={() => depositMoney(100)}>Deposit</button>
+      <button onClick={() => withdrawMoney(100)}>Withdraw</button>
+      <button onClick={() => bankrupt()}>bankrupt</button>
     </div>
   )
 }
